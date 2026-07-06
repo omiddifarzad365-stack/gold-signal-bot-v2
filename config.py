0e4dd7e6a768c3df1861551f9c376aced396e54b
omiddifarzad365-stack <omiddifarzad365@gmail.com>
2026-07-06 20:47:19 +0330

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
+TWELVEDATA_API_KEY = os.getenv("TWELVEDATA_API_KEY")
+
+SYMBOL = "XAU/USD"
+INTERVAL = "5min"
+TIMEZONE = "Asia/Tehran"
