5b4d6fea4b68793d4f85ee068de53e0562c989fe
omiddifarzad365-stack <omiddifarzad365@gmail.com>
2026-07-06 22:24:09 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
-from config import *
+import requests
+from config import BOT_TOKEN, CHAT_ID
 
-print("Gold Signal Bot Started...")
+def send_message(text):
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+    data = {"chat_id": CHAT_ID, "text": text}
+    requests.post(url, data=data)
+
+send_message("🚀 Bot Started Successfully")
